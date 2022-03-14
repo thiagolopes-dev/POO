10d9c77e8d16cf8f359fed172233dc2f1d70fbfe
Thiago Lopes <thiagoolopezz@gmail.com>
2022-03-13 22:54:12 -0300

    Aula 3 Melhorias

--- main.ts
+++ main.ts
@@ -5,7 +5,7 @@ let person: Personagem = new Personagem('Goku', 10, 20, 30, 40);
 let teclado = prompt();
 let option: number = 0;
 
-while (option !== 9) {
+while (option !== 9 || person.isDead() ) {
     console.log('===========Personagem============');
     console.log('1. Treinar Ataque                ');
     console.log('2. Treinar Defesa                ');
@@ -18,78 +18,29 @@ while (option !== 9) {
     option = +teclado('Ecolha uma ação: ');
 
     switch (option) {
-        // ataque
         case 1:
-            person.ataque += Math.random() * 7;
-            person.energia -= Math.random() * 10;
-            if (person.energia < 0) {
-                console.log('Deu ruim, Você morreu!');
-            }
-            console.log('Guerreiro: ');
-            console.log('Nome: ', person.nome);
-            console.log('Energia: ', person.energia.toFixed(1));
-            console.log('Ataque: ', person.ataque.toFixed(1));
-            console.log('Defesa: ', person.defesa.toFixed(1));
-            console.log('Vida: ', person.vida.toFixed(1));
+            person.treinarAtaque();
+           console.log(person.status());
             break;
-
-        // defesa
         case 2:
-            person.defesa += Math.random() * 5;
-            person.energia -= Math.random() * 10;
-            if (person.energia < 0) {
-                console.log('Deu ruim, Você morreu!');
-            } else {
-                if (person.defesa > 100) {
-                    person.defesa = 0;
-                }
-            }
-            console.log('Guerreiro: ');
-            console.log('Nome: ', person.nome);
-            console.log('Energia: ', person.energia.toFixed(1));
-            console.log('Ataque: ', person.ataque.toFixed(1));
-            console.log('Defesa: ', person.defesa.toFixed(1));
-            console.log('Vida: ', person.vida.toFixed(1));
+            person.treinarDefesa();
+           console.log(person.status());
             break;
-
-        // descansar
         case 3:
-            person.energia += Math.random() * 10;
-            if(person.energia > 100) {
-                person.energia = 100;
-            }
-            console.log('Guerreiro: ');
-            console.log('Nome: ', person.nome);
-            console.log('Energia: ', person.energia.toFixed(1));
-            console.log('Ataque: ', person.ataque.toFixed(1));
-            console.log('Defesa: ', person.defesa.toFixed(1));
-            console.log('Vida: ', person.vida.toFixed(1));
+            let horas: number = +teclado('Digite o número de horas: ');
+            person.descansar();
+           console.log(person.status());
             break;
-
-        // batalha
         case 4:
-            person.energia -= Math.random() * 100;
-            if (person.energia < 0) {
-                console.log('Deu ruim, Você morreu!');
-            }
-            console.log('Guerreiro: ');
-            console.log('Nome: ', person.nome);
-            console.log('Energia: ', person.energia.toFixed(1));
-            console.log('Ataque: ', person.ataque.toFixed(1));
-            console.log('Defesa: ', person.defesa.toFixed(1));
-            console.log('Vida: ', person.vida.toFixed(1));
+            let rest: number = person.batalhar();
+            console.log(`Esta batalha custou ${rest} de energia`);
+            console.log(person.status());
             break;
-
         case 8:
-            console.log('Guerreiro: ');
-            console.log('Nome: ', person.nome);
-            console.log('Energia: ', person.energia.toFixed(1));
-            console.log('Ataque: ', person.ataque.toFixed(1));
-            console.log('Defesa: ', person.defesa.toFixed(1));
-            console.log('Vida: ', person.vida.toFixed(1));
+           console.log(person.status());
             break;
-
         default:
             break;
     }
 }
+console.log('Você morreu !');
--- personagem.ts
+++ personagem.ts
@@ -7,7 +7,49 @@ export class Personagem {
         public vida: number
 
     ) { }
+
+
+    // Toda vez que pensar em leitura de dados na classe - use parametros
+
+    // Toda vez que pensar em imprimir na classe, use retorno
+
+    status(): string {
+        return (
+            "Guerreiro:  \n" +
+            "\nNome: " +
+            this.nome +
+            ("\nEnergia: " + this.energia.toFixed(1)) +
+            ("\nAtaque: " + this.ataque.toFixed(1)) +
+            ("\nDefesa: " + this.defesa.toFixed(1))
+        );
+    }
+    treinarAtaque(): void {
+        this.ataque += Math.random() * 7;
+        this.energia -= Math.random() * 10;
+        if (this.defesa > 100) {
+            this.defesa = 0;
+        }
+    }
+    treinarDefesa(): void {
+        this.defesa += Math.random() * 5;
+        this.energia -= Math.random() * 10;
+        if (this.defesa > 100) {
+            this.defesa = 0;
+        }
+    }
+    descansar(horas: number): void {
+        this.energia += horas * (Math.random() * 10);
+        if (this.energia > 100) {
+            this.energia = 100;
+        }
+    }
+    batalhar(): number {
+        let percaBatalha: number = Math.random() * 100;
+        this.energia -= percaBatalha;
+        return percaBatalha;
+    }
+    isDead(): boolean {
+       return this.energia < 0;
+    }
+
 }
-
-
-
