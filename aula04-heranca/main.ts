a798b7296d77d0e969be1809462b0cde9e9ed349
Thiago Lopes <thiagoolopezz@gmail.com>
2022-03-27 11:42:07 -0300

    aula herança

---
+++ aula04-heranca/main.ts
@@ -0,0 +1,13 @@
+import { Guerreiro } from "./guerreiro";
+import { Mago } from "./mago";
+import { Personagem } from "./personagem";
+import { Sacedorte } from "./sacedorte";
+
+
+let mago: Personagem = new Mago("Gandalf Mago");
+let guerreiro: Personagem = new Guerreiro("Aragorn Guerreiro");
+let sacedorte: Personagem = new Sacedorte("Bilbo Sacedorte");
+
+console.log("Mago 🧙", mago);
+console.log("Guerreiro 🗡️", guerreiro);
+console.log("Sacedorte 👲", sacedorte);
